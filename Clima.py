cadfc47bd7a0ab04f44d1304f97137d12a01d2ee
erickisraelg <101534491+erickisraelg@users.noreply.github.com>
2022-03-25 05:45:28 -0300

    Update Clima.py

--- Clima.py
+++ Clima.py
@@ -10,18 +10,3 @@ clima = str(soup.find_all(class_='bloco-previsao d-flex flex-column text-center'
 print(f'~ Temp. Máxíma: {clima[112:115]} Graus Celsius\n'
       f'~ Temp. Minima: {clima[175:178]} Graus Celsius\n'
       f'~ Humidade: {clima[312:314]} \n')
-
-
-
-
-
-
-
-
-
-
-"""for listagem in data:
-    if cidade in listagem:
-        print(bool(listagem))"""   #MOSTRAR A CIDADE[TRUE OU FALSE]
-
-
